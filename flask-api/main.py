9ceb51aa21ed65e2e0be55c1779e61dad44ee149
Yuki Nagae <yuki.nagae1130@gmail.com>
2019-08-25 22:37:41 +0900

    Add flask-api files

---
+++ flask-api/main.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask, jsonify
+import pymysql
+
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
+from opencensus.ext.stackdriver import trace_exporter as stackdriver_exporter
+from opencensus.trace import config_integration
+from opencensus.trace import samplers
+
+
+app = Flask(__name__)
+
+INTEGRATIONS = ["pymysql"]
+exporter = stackdriver_exporter.StackdriverExporter()
+sampler = samplers.ProbabilitySampler(rate=1)
+middleware = FlaskMiddleware(app, exporter=exporter, sampler=sampler)
+config_integration.trace_integrations(INTEGRATIONS)
+
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_USER = os.getenv("DB_USER")
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/liveness')
+def liveness():
+    return jsonify(status="OK", route="liveness")
+
+@app.route('/readiness')
+def readiness():
+    conn = pymysql.connect(host=DB_HOST, port=3306, user=DB_USER, db='mysql')
+    conn.close()
+    return jsonify(status="OK", route="readiness")
